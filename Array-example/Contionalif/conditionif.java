b58650e445a4d0a749cd21df58e8a8ec2d1ded52
256496 <maheshkeskar77930@gmail.com>
2023-02-03 20:13:13 +0530

    Adding to if pogramm

---
+++ Array-example/Contionalif/conditionif.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+    public class conditionif {
+        public static void main(String[] args) {
+            int password;
+            System.out.println("Enter password");
+
+            Scanner sc = new Scanner(System.in);
+            password = sc.nextInt();
+
+            if (password == 7383) {
+
+                System.out.println("my name mahesh");
+            }
+        }
+    }
+
